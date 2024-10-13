307e6b40758cebdbbdb2f75e0dd1b9e615325ab6
Harsh-Prajapati02 <harsh.prajapati0206@gmail.com>
2024-10-13 18:58:48 +0530

    update

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -34,8 +34,7 @@ const Collaboration = () => {
           <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
             {collabText}
           </p>
-
-          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale-75 md:scale-100">
             <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
               <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
                 <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
